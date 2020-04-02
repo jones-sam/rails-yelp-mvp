c49a2436f38c6b3b34207fe4d173b27053ecd0c7
jones-sam <samjones6058@gmail.com>
2020-04-02 12:45:10 -0300

    added content to view, added routes

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+   end
+
+  def new; end
+
+  def create; end
+end
